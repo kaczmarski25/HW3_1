a6a5302bfd2865439a08aa6328812f9c44974379
kaczmarski <eukaczmarski@gmail.com>
2022-05-03 10:47:50 +0300

    Hw3.1

---
+++ src/geekbrains_home_work/Apple.java
@@ -0,0 +1,8 @@
+package geekbrains_home_work;
+
+public class Apple extends Fruit {
+    private static final float weight = 1.0f;
+    public Apple(){
+        super.setWeight(weight);
+    }
+}
---
+++ src/geekbrains_home_work/Fruit.java
@@ -0,0 +1,13 @@
+package geekbrains_home_work;
+
+public abstract class Fruit {
+    private float weight;
+
+    public void setWeight (float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight(){
+        return weight;
+    }
+}
